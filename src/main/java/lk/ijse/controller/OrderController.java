93416c324a63ccc95bb7bbeef5e73b3c713d7cb5
chulakasam <sampathchulaka33872@gmail.com>
2023-11-16 15:49:33 +0530

    now go

--- src/main/java/lk/ijse/controller/OrderController.java
+++ src/main/java/lk/ijse/controller/OrderController.java
@@ -1,18 +1,23 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.scene.Cursor;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lk.ijse.dto.ClientDto;
 import lk.ijse.dto.ItemDto;
+import lk.ijse.dto.Tm.CartTm;
 import lk.ijse.model.ClientModel;
 import lk.ijse.model.ItemModel;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderController {
     public Label lblDate;
@@ -21,16 +26,29 @@ public class OrderController {
     public Label txtClientname;
     public Label lblItemName;
     public Label lblPrice;
+    public TableColumn <?,?> colQty;
+    public TextField txtQty;
+    public Label lblNetTotal;
+    public TableColumn <?,?> colItemcode;
+    public TableColumn <?,?> coldescription;
+    public TableColumn <?,?>colUnitprice;
+    public TableColumn<?,?> colTotal;
+    public TableColumn<?,?> colAction;
+    public TableView <CartTm>tblOrderCart;
+    public JFXButton btnAddtoCart;
+    public ObservableList<CartTm> obList = FXCollections.observableArrayList();
+
+
 
     public void setDate(){
        lblDate.setText(String.valueOf(LocalDate.now()));
     }
     public  void initialize(){
+        setCellValueFactory();
         setDate();
         LoadAllClients();
         loadAllItem();
     }
-
     public void LoadAllClients() {
         ObservableList<String> obList = FXCollections.observableArrayList();
 
@@ -60,7 +78,6 @@ public class OrderController {
         }
 
     }
-
     public void cmbClientOnAction(ActionEvent actionEvent) throws SQLException {
         String Id = cmbClient.getValue();
         try{
@@ -71,7 +88,6 @@ public class OrderController {
             throw  new RuntimeException(e);
         }
     }
-
     public void cmbItemOnAction(ActionEvent actionEvent) {
         String code = cmbItem.getValue();
 
@@ -83,5 +99,75 @@ public class OrderController {
         }catch (SQLException e){
             throw  new RuntimeException(e);
         }
+    }
+
+    public void btnAddTocartOnAction(ActionEvent actionEvent) {
+        String code = cmbItem.getValue();
+        String description = lblItemName.getText();
+        double unitPrice = Double.parseDouble(lblPrice.getText());
+        int qty = Integer.parseInt(txtQty.getText());
+        double tot=unitPrice * qty;
+        Button btn=new Button("DELETE");
+
+        setRemoveBtnAction(btn);
+        btn.setCursor(Cursor.HAND);
+
+        if (!obList.isEmpty()) {
+            for (int i = 0; i < tblOrderCart.getItems().size(); i++) {
+                if (colItemcode.getCellData(i).equals(code)) {
+                    int col_qty = (int) colQty.getCellData(i);
+                    qty += col_qty;
+                    tot = unitPrice * qty;
+
+                    obList.get(i).setQty(qty);
+                    obList.get(i).setTot(tot);
+
+                    calculateTotal();
+                    tblOrderCart.refresh();
+                    return;
+                }
+            }
+        }
+        var cartTm = new CartTm(code, description, qty, unitPrice, tot, btn);
+
+        obList.add(cartTm);
+
+        tblOrderCart.setItems(obList);
+        calculateTotal();
+        txtQty.clear();
+    }
+
+    private void setRemoveBtnAction(Button btn) {
+        btn.setOnAction((e) -> {
+            ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+            ButtonType no = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+            Optional<ButtonType> type = new Alert(Alert.AlertType.INFORMATION, "Are you sure to remove?", yes, no).showAndWait();
+
+            if (type.orElse(no) == yes) {
+                int focusedIndex = tblOrderCart.getSelectionModel().getSelectedIndex();
+
+                obList.remove(focusedIndex);
+                tblOrderCart.refresh();
+                calculateTotal();
+            }
+        });
+    }
+
+    private void calculateTotal() {
+        double total = 0;
+        for (int i = 0; i < tblOrderCart.getItems().size(); i++) {
+            total += (double) colTotal.getCellData(i);
+        }
+        lblNetTotal.setText(String.valueOf(total));
+    }
+
+    private void setCellValueFactory(){
+        colItemcode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        coldescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colUnitprice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        colTotal.setCellValueFactory(new PropertyValueFactory<>("tot"));
+        colAction.setCellValueFactory(new PropertyValueFactory<>("btn"));
     }
 }
